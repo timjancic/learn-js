bce41edcc3a00e939387b7c85e33d4429ffbc65d
timjancic <timjancic.gravity@gmail.com>
2021-10-13 15:32:54 -0500

    Finished functionality of result function and added variables to create story

--- silly-story-generator.js
+++ silly-story-generator.js
@@ -6,3 +6,41 @@ function randomValueFromArray(array){
   const random = Math.floor(Math.random()*array.length);
   return array[random];
 }
+
+let storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day";
+let insertX = ['Willy the Goblin','Big Daddy','Father Christmas'];
+let insertY = ['the soup kitchen','Disneyland','the White House'];
+let insertZ = ['spontaneously combusted','melted into a puddle on the sidewalk','turned into a slug and crawled away'];
+
+randomize.addEventListener('click', result);
+
+function result() {
+  let newStory = storyText;
+
+  if(customName.value !== '') {
+    let name = customName.value;
+
+    newStory = newStory.replace("Bob",name)
+  }
+
+  if(document.getElementById("uk").checked) {
+    let weight = Math.round(300 * 0.071428571428571) + ' stones';
+    let temperature =  Math.round((94 - 32)/1.8) + ' centigrade';
+
+    newStory = newStory.replace("300 pounds",weight);
+    newStory = newStory.replace("94 fahrenheit",temperature);
+  }
+
+  let xItem = randomValueFromArray(insertX);
+  let yItem = randomValueFromArray(insertY);
+  let zItem = randomValueFromArray(insertZ);
+
+  newStory = newStory.replace(':insertx:',xItem);
+  newStory = newStory.replace(':insertx:',xItem);
+  newStory = newStory.replace(':inserty:',yItem);
+  newStory = newStory.replace(':insertz:',zItem);
+
+  story.textContent = newStory;
+  story.style.visibility = 'visible';
+
+}
